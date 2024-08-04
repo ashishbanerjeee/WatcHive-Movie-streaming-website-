0f261f598636fa754906cc0d4e9051bce727a680
ashishbanerjeee <141298586+ashishbanerjeee@users.noreply.github.com>
2024-08-04 13:07:55 +0530

    WatcHive
    WatcHive is a dynamic movie streaming website offering a vast collection of films for every taste. Features include seamless ad integration, user-friendly movie uploading, and comprehensive admin pages for efficient site management. Enjoy an unparalleled viewing experience with WatcHive's intuitive and innovative platform.

---
+++ All Codes/README.md
@@ -0,0 +1,67 @@
+# OnlineMovieStreaming
+
+# Functionalities :
+
+           - login system : create acc, delete acc, update user details
+           - watching movies/videos
+           - latest updated videos
+           - most viewed videos
+           - search functionality
+
+- the one to signup first is the admin
+- only the admin has the rights to upload movies/videos and images.
+
+# Technologies used :
+
+- HTML, CSS, Bootstrap for frontend
+- PHP and MySQL for backend
+
+# Before use :
+
+- truncate all the tables after importing them from the sql-files folder & edit the database name in dbh.php
+- create a "uploads" folder to store the uploaded images/posters of the movie/video.
+- create a "video-uploads" folder to store uploaded videos of the corresponding image.
+- enter 1st row of the table manually and transfer video and image to the respective folders.
+
+# Screenshots :
+
+Online Movie Streaming in PHP with Full Source Code
+
+The Online Movie Streaming is a web based system that is made up of PHP, JavaScript, CSS and MyQSL for the database. It contains two sides; the user side and the admin side. It is so simple and designed as a user friendly system. The admin plays an important role in this Online Movie Streaming in PHP with Full Source Code.
+
+About the Online Movie Streaming in PHP with Full Source Code
+
+Because of our current situation nowadays, most of us are just a home relaxing and loves to watch online movie shows or any movies that are free of charge. So, this Online Movie Streaming in PHP with Full Source Code will make it easier for you to watch your favorite movies online. All you have to do is simply make an account by signing up a form in the system. Once you are done signing up you can now log in into the system by providing your email address and password. Then, you will see there all the movies that you want to watch. You can even search for different kinds of movie genres, you can see the latest uploaded movies and most watched movies. While on the admin side, he has the ability to update and add the movies in this Online Movie Streaming in PHP with Full Source Code.
+
+These are the following features of the Online Movie Streaming in PHP with Full Source Code
+
+    Upload Movies
+    Search Movies
+    Watch Movies
+    Sign Up
+    Login
+
+
+    # How to setup the Online Movie Streaming in PHP with Full Source Code
+    Download the zip file.
+    Download and install XAMPP
+    Run the XAMPP control panel and start MySQL and Apache
+    Go to C:\xampp\htdocs and extract the downloaded zip file (onlinemovies) inside the folder
+    Open the browser and go to http://localhost/phpmyadmin/ to create the database
+    Click the new to create a database.
+    Name the database sourcecodester_ omsdb.
+    Click import to import the sql file.
+    Click choose file and select the file that can be found inside the onlinemovies folder
+    Click go.
+
+How to run the Online Movie Streaming in PHP with Full Source Code
+
+Open the browser and go to http://localhost/onlinemovies/
+
+Accessing admin accounts
+
+Username: admin@gmail.com
+Password: admin
+
+Client :krushna@gmail.com
+Password :1234
---
+++ All Codes/login.php
@@ -0,0 +1,98 @@
+<?php
+session_start();
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>WatcHive</title>
+    <link rel="stylesheet" href="master.css" type="text/css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  </head>
+  <body>
+    <header>
+      <div class="container-fluid " >
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark " style="margin-left: -15px; width:102%;" >
+            <a href="login.php" class="navbar-brand"> <img src="images/Logo3.png" alt=""> </a>
+            <span class="navbar-text">WatcHive</span>
+
+            <ul class="navbar-nav">
+              <li class="nav-item"> <a href="#A" class="nav-link"> Services</a> </li>
+              <li class="nav-item"> <a href="#B" class="nav-link"> Advertisement</a> </li>
+              <li class="nav-item"> <a href="user-login.php" class="nav-link"> Sign In</a> </li>
+
+            </ul>
+
+        </nav>
+
+        <div class="container" style="width:300px">
+          <div class="jumbotron">
+            <h1>Watch Anywhere, <br> Watch Anytime... </h1> <br>
+            <a href="test.php" type="button" class="btn btn-danger btn-block">Sign Up Now</a>
+          </div>
+        </div>
+      </div>
+
+      </header>
+
+
+
+    <section class="features">
+        <a href="#" name="A"></a>
+        <h2>Our Services</h2>
+
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4">
+              <p class="arrange"><img src="images/mob.png" alt=""> <br> WatcHive is a premier online movies website that caters to cinephiles worldwide, offering a vast library of films spanning various genres and languages. The platform provides high-quality streaming, ensuring viewers enjoy a seamless and immersive experience. With user-friendly navigation and personalized recommendations, WatcHive makes it easy to discover new favorites and revisit classics. The site features the latest releases and timeless masterpieces, appealing to diverse tastes. Additionally, WatcHive's subscription plans are flexible and affordable, catering to different viewing needs. Its commitment to delivering excellent customer service and maintaining a rich, constantly updated collection sets it apart in the crowded online streaming market.
+              </p>
+            </div><div class="col-md-4">
+              <p class="arrange"><img src="images/mess.png" alt=""> <br> WatcHive is set to revolutionize online movie streaming by offering an extensive collection of films across all genres, ensuring there is something for everyone. We will provide high-definition streaming with minimal buffering, delivering an optimal viewing experience. Our platform will feature user-friendly navigation and personalized recommendations, helping users discover new favorites effortlessly. Additionally, we plan to include exclusive content, early access to new releases, and behind-the-scenes features. Flexible subscription plans will cater to various budgets, and our dedicated customer support team will be available 24/7 to assist with any issues. WatcHive aims to be the ultimate destination for movie enthusiasts.</p>
+            </div>
+              <div class="col-md-4">
+
+                <p class="arrange">
+                  <img src="images/desktop.jpg"> <br>  WatcHive is currently available exclusively in website form to provide users with a stable, high-quality movie streaming experience that leverages the robust capabilities of web browsers. This approach ensures compatibility across various devices, including desktops, laptops, and tablets, without the need for additional app downloads. By focusing on a web-based platform, we can deliver swift updates and improvements, maintaining optimal performance and security. Furthermore, our website allows for seamless integration of advanced features like personalized recommendations and user-friendly navigation, ensuring an enjoyable and hassle-free viewing experience. This strategic choice ensures that WatcHive remains accessible and convenient for all users.</p>
+              </div>
+
+            </div>
+
+          </div>
+          <h3></h3>
+
+    </section>
+
+    <section class="features">
+        <a href="#" name="B"></a>
+        <h2>Advertisement</h2>
+
+        <div class="container">
+          <div class="row">
+            <!-- <div class="col-md-4">
+              <p class="arrange"><img src="images/mob.png" alt=""> <br> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div> -->
+            <div class="col-md-4">
+              <p class="arrange"><img src="images/WatcHiveQR.png" alt=""> <br> WatcHive offers targeted advertisements that enhance the viewing experience by promoting relevant films and products. Our ad strategy ensures minimal disruption, integrating seamlessly into the platform. Advertisers benefit from a focused audience, while users enjoy content recommendations that align with their interests, making ad interactions both useful and engaging.<br>
+            <br>Follow the Steps Below : <br> 
+            <br>1. Scan the QR Code .<br>
+            <br>2. A link will Open click on that link . <br>
+            <br>3. It will open directly to WhatsApp .<br>
+            <br>4. Over their you can connect for Advertisement.
+
+          </p>
+              <div class="col-md-4">
+
+                
+            </div>
+    <footer class="page-footer font-small blue">
+
+    <!-- <div class="footer-copyright text-center py-3">© 2024 Copyright:
+      <a href=""> ___WatcHive___</a>
+    </div> -->
+
+    </footer>
+  </body>
+</html>
